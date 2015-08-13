0f7b2a5cb07acce3d2dd8cbd81ca5d009e07e932
Paul Healy <lmiphay@gmail.com>
2015-08-13 22:59:51 +0100

    add -v visualiser option to example

--- media-sound/squeezelite/files/squeezelite.conf.d
+++ media-sound/squeezelite/files/squeezelite.conf.d
@@ -1,11 +1,13 @@
 # Copyright 1999-2015 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: $
 
 # /etc/conf.d/squeezeslave: configuration for /etc/init.d/squeezelite
 
 # Switches to pass to Squeezelite. See 'squeezelite -h' for
 # a description of the possible switches.
-# Example setting the server IP, the ALSA output device and the player name:
-# SL_OPTS="-s 192.168.1.56 -o sysdefault -n $HOSTNAME"
+#
+# Example setting the server IP, the ALSA output device, the player name
+# and visualiser support:
+# SL_OPTS="-s 192.168.1.56 -o sysdefault -n $HOSTNAME -v"
+#
 SL_OPTS=""
