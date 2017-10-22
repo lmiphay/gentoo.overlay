c1fd4ef1fd9078fa218c0002535baf00a1f09ca6
Paul Healy <lmiphay@gmail.com>
2017-10-22 12:34:25 +0100

    revbump squeezelite to latest available

--- media-sound/squeezelite/files/squeezelite.conf.d
+++ media-sound/squeezelite/files/squeezelite.conf.d
@@ -1,4 +1,4 @@
-# Copyright 1999-2016 Gentoo Foundation
+# Copyright 1999-2017 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
 # /etc/conf.d/squeezeslave: configuration for /etc/init.d/squeezelite
@@ -8,10 +8,6 @@
 #
 # Example setting the server IP, the ALSA output device, the player name
 # and visualiser support:
-# SL_OPTS="-s 192.168.1.56 -o sysdefault -n $HOSTNAME -v"
-#
-# Example seleting pulse output:
-# export PULSE_SERVER=localhost
-# SL_OPTS="-s 192.168.1.56 -o pulse -n $HOSTNAME -v"
+# SL_OPTS="-s 192.168.1.56 -o sysdefault -n $HOSTNAME"
 #
 SL_OPTS=""
